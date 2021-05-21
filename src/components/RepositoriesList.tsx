54338c4f1ea36e4650260c9101d9776b57fe7ab7
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-05-21 17:10:45 -0400

    completed RepositoriesList component; added type to react-redux useSelector to get it to work with TS

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  ); // gets state from store; select what we need
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    searchRepositories(term); // already bound to dispatch in hook
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      <ul>
+        {!error &&
+          !loading &&
+          data.map((name, key) => {
+            return <li key={key}>{name}</li>;
+          })}
+      </ul>
     </div>
   );
 };
